ac2a807df36a95dd504c47cbafb322a6ac9408a6
Xinyuan Zhao <2292289473@qq.com>
2023-02-11 20:55:12 +0800

    style: 优化注释

--- server/sway-camp/src/main/java/com/zxy/swaycamp/common/constant/RegexpConst.java
+++ server/sway-camp/src/main/java/com/zxy/swaycamp/common/constant/RegexpConst.java
@@ -7,9 +7,13 @@ package com.zxy.swaycamp.common.constant;
  * @since 2023/2/11
  */
 public class RegexpConst {
+
+    /**
+     * 邮箱正则
+     */
     public static final String REGEXP_EMAIL = "[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+";
+    /**
+     * 手机正则
+     */
     public static final String REGEXP_PHONE = " ^[1][3,4,5,6,7,8,9][0-9]{9}$";
-
-
-
 }
